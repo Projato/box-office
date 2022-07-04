b372fe885e4c91b2439312aaece65bfc884dccdd
lazarus <projatochakraborty@gmail.com>
2022-07-04 20:50:41 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>bal</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+
+      <Route exact path="/starred">
+        This is starred
+      </Route>
+
+      <Route>You-re not supposed to be here</Route>
+    </Switch>
+  );
 }
 
 export default App;
