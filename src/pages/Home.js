dd27a9f165801625322b01c868399f11e3f940f0
lazarus <projatochakraborty@gmail.com>
2022-07-04 22:20:44 +0530

    created input and etcg results from tv maze api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,42 @@
-/* eslint-disable react/function-component-definition */
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  return <MainPageLayout>this is home</MainPageLayout>;
+  const [input, setInput] = useState('');
+
+  const onSearch = () => {
+    // https://api.tvmaze.com/search/shows?q=men
+
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
